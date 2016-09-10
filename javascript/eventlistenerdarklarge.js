99c194dfc576564352c48349f4a3792ed05d76a7
Debra Gordon <debra.m.gordon@gmail.com>
2016-09-10 13:57:47 -0500

    added event listenters dor dark and large

---
+++ javascript/eventlistenerdarklarge.js
@@ -0,0 +1,18 @@
+var mainElement = document.getElementsByTagName("body");
+
+function makeItDark() {
+  mainElement[0].classList.toggle("dark");
+}
+
+var darkenThings = document.getElementById("choice-dark");
+darkenThings.addEventListener('click', makeItDark);
+
+
+// function to make large
+
+function makeItLarge() {
+  mainElement[0].classList.toggle("large");
+}
+
+var enlargeThings = document.getElementById("choice-large");
+enlargeThings.addEventListener('click', makeItLarge);
