3d3bb272a9891c586fd02404a9b63c87355f3dff
Taylor Shuff <tkylesh@gmail.com>
2016-09-13 20:29:57 -0500

    beautified css file

--- javascript/chattyaddmessage.js
+++ javascript/chattyaddmessage.js
@@ -30,7 +30,7 @@ var Chatty = (function(addChatty)
 			newMessages += `<div id='message-${[i]}'>`;
 	    	newMessages += `<p>${currentMessage}</p>`;
 	    	newMessages += `<p class="tiny">time written: ${currentTime}</p>`;
-	    	newMessages += "<button id='deleteBtn' type='button'>Delete</button><br/>";
+	    	newMessages += "<button id='deleteBtn' type='button' class='btn'>Delete</button><br/>";
 	    	newMessages +="</div>";
 		}
 		printTo.innerHTML =  newMessages;	
